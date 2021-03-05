5aabc67b2e3cde23448c7a4cd01b26cbd8684389
YiannisFy <77490996+YiannisFy@users.noreply.github.com>
2021-03-05 15:26:15 +0200

    Redundant code removal

--- src/redux/init.js
+++ src/redux/init.js
@@ -1,19 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './TetrisGame';
 
-/**
- * Holds the store slices for all games.
- */
-let slices = new Map();
-
-/**
- * The Redux store.
- */
 export let store;
-
-export function getSliceForGame(gameId = "") {
-	return slices.get(gameId);
-}
 
 /**
  * Setups the Redux store.
